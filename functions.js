32bac0a309d8f8c34c371d16db81bc1d866bd809
Xander Turnbull <xanderturnbull2017@gmail.com>
2019-11-07 11:13:34 +0000

    Removed JS changed to select

--- functions.js
+++ functions.js
@@ -1,20 +1,3 @@
-function showMenu() {
-    document.getElementById("drop-items").classList.toggle("show");
-
-    window.onclick = function (event) {
-        if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown__content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show')
-                }
-            }
-        }
-    }
-}
-
 document.querySelector('.c-nav__burger').addEventListener('click', () => {
     const navInner = document.querySelector('.c-nav__inner');
     navInner.classList.toggle('c-nav__inner--active')
