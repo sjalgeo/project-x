90fd7f4b599b7eac45e62afd11ce19e6d356ea2a
Xander Turnbull <xanderturnbull2017@gmail.com>
2019-11-27 12:42:08 +0000

    Uneeded comment removed

--- functions.js
+++ functions.js
@@ -4,9 +4,8 @@ document.querySelector(".c-nav__burger").addEventListener("click", () => {
 });
 
 const nav = document.querySelector(".c-nav");
-window.addEventListener("scroll", () => {
-  //   const scroll = ;
 
+window.addEventListener("scroll", () => {
   window.scrollY >= 20
     ? nav.classList.add("c-nav--alt")
     : nav.classList.remove("c-nav--alt");
